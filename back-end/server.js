7744e212d07d0d9be290332e288f381617c78e42
Hernanne Domiciano <78621976+hernannegp@users.noreply.github.com>
2021-11-05 10:59:32 -0300

    feat: faz a conexão com o banco via mongoose e adiciona o crud no modelo msc

--- back-end/server.js
+++ back-end/server.js
@@ -1,9 +1,38 @@
 const express = require('express');
 
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const mongoose = require('mongoose');
+const Todo = require('./models/todoModel');
+const todoController = require('./controller/todoController');
+
+mongoose.connect('mongodb://127.0.0.1:27017/todolist', { useNewUrlParser: true });
+
+mongoose.connection.once('open', () => {
+  global.console.log('banco de dados online');
+});
+
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get('/', todoController.getTodos);
+
+app.post('/', todoController.createTodo);
+
+app.get('/:id', (req, res) => {
+  const { id } = req.params;
+  Todo.findById(id, (_err, todo) => {
+    res.json(todo);
+  });
+});
+
+app.put('/:id', todoController.updateTask);
+app.delete('/:id', todoController.deleteTask);
+
 app.listen(port, () => {
   const message = `Example app listening on ${port}`;
   global.console.log(message);
